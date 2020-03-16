0ea43515b6d752ca1d9b5098adad371a875ead11
wStockhausen <william.stockhausen@noaa.gov>
2020-03-16 07:55:39 -0700

    Corrections to getStandardBoundingBox for names with lower case.

--- R/getStandardBoundingBox.R
+++ R/getStandardBoundingBox.R
@@ -20,13 +20,13 @@ getStandardBoundingBox<-function(area=c("EBS","Bering10k","CGOA","CGOA shelf")){
   if (toupper(area[1])=="EBS")
     return(list(bottomleft=list(lon=-179,lat=54),
                 topright  =list(lon=-157,lat=62.5)));
-  if (toupper(area[1])=="Bering10k")
+  if (toupper(area[1])==toupper("Bering10k"))
     return(list(bottomleft=list(lon=156.86111,lat=45.07368),
                 topright  =list(lon=214.98959,lat=69.54647)));
   if (toupper(area[1])=="CGOA")
     return(list(bottomleft=list(lon=-162.67109,lat=46.68325),
                 topright  =list(lon=-132.13292,lat=64.15562)));
-  if (toupper(area[1])=="CGOA shelf")
+  if (toupper(area[1])==toupper("CGOA shelf"))
     return(list(bottomleft=list(lon=-162.75,lat=54.5),
                 topright  =list(lon=-135.70,lat=62.2)));
 }
